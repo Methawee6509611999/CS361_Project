ec0cbf1729a9b7cc2861ab4761a1681b83039696
natcha6509611676 <natcha.wattan@dome.tu.ac.th>
2024-11-21 20:59:13 +0700

    หน้าแรกผ่าน, หน้าวิดิโอผ่าน daylight เล่นได้

--- app/src/main/java/com/example/warewolf/DayCount.java
+++ app/src/main/java/com/example/warewolf/DayCount.java
@@ -1,10 +1,7 @@
 package com.example.warewolf;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -14,27 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class DayCount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.noon_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.sunlight), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        final Button start_btn = findViewById(R.id.startButton);
+        VideoView sunVideoView = findViewById(R.id.sunlight);
+        sunVideoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.sun_light));
+        sunVideoView.setMediaController(new MediaController(this));
+        sunVideoView.requestFocus();
 
-        start_btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(MainActivity.this, DayCount.class);
-                startActivity(intent);
-            }
-        });
     }
 }
+
+
